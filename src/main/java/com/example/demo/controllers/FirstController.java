22c518b617e6a372830d805f70cab226fa3ad665
detayaa <drugochenko.taya@mail.ru>
2022-12-15 10:38:04 +0700

    1

--- src/main/java/com/example/demo/controllers/FirstController.java
+++ src/main/java/com/example/demo/controllers/FirstController.java
@@ -4,6 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public class FirstController {
+    @GetMapping
+    public String hello() {
+        return "Приложение запущено";
+    }
+    @GetMapping("/info")
+    public String infromation(@RequestParam String name, String nameProject, String date, String describe) {
+        return "Меня зовут " + name + ", название проекта " + nameProject + ", дата - " + date + ", описание: " + describe;
+    }
 
 }
+
 

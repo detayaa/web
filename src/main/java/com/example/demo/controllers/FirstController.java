c86f427f96657b6037e4adf7e4b83f5f354337cf
detayaa <drugochenko.taya@mail.ru>
2022-12-17 17:19:20 +0700

    1

--- src/main/java/com/example/demo/controllers/FirstController.java
+++ src/main/java/com/example/demo/controllers/FirstController.java
@@ -2,6 +2,9 @@ package com.example.demo.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 
 public class FirstController {
     @GetMapping

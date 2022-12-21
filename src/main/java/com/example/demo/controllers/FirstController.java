d72b98fd6ebae5f1295cc4c34e4db320959b0c22
detayaa <drugochenko.taya@mail.ru>
2022-12-21 11:17:04 +0700

    1

--- src/main/java/com/example/demo/controllers/FirstController.java
+++ src/main/java/com/example/demo/controllers/FirstController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 
 public class FirstController {
-    @GetMapping
+    @GetMapping("/hello")
     public String hello() {
         return "Приложение запущено";
     }
